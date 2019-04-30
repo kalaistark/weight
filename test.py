f6acc3a3b24654ca26b18346077c286b2c6b319b
kalaistark <kalaiselvant0@gmail.com>
2019-04-30 10:45:17 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,60 @@
+import sys, subprocess, serial, csv
+import os
+import datetime as time
+from PyQt5 import QtWidgets
+
+port = "/dev/ttyUSB0"
+
+# subprocess.run("chmod 666 /dev/ttyUSB0")
+returned = os.popen("sudo chmod 666 /dev/ttyUSB0").readline()
+print(returned)
+
+ser = serial.Serial("/dev/ttyUSB0", 2400, timeout=2)
+
+
+#dmesg | grep tty -- to list serial ports in linux
+# sudo chmod 666 /dev/ttyUSB0)
+
+class Window(QtWidgets.QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.l = QtWidgets.QLabel("Weight")
+        self.btn = QtWidgets.QPushButton("Get Weight")
+        self.init_ui()
+
+    def init_ui(self):
+        h_box = QtWidgets.QHBoxLayout()
+        h_box.addStretch()
+        h_box.addWidget(self.l)
+        h_box.addStretch()
+
+        v_box = QtWidgets.QVBoxLayout()
+        v_box.addLayout(h_box)
+        v_box.addWidget(self.btn)
+
+        self.setLayout(v_box)
+
+        self.setWindowTitle("Weight App")
+        self.resize(500, 300)
+
+        self.btn.clicked.connect(self.get_weight)
+
+        self.show()
+
+    def get_weight(self):
+        ser.flushOutput()
+        bytesToRead = ser.inWaiting()
+        v= str(ser.read(bytesToRead))
+        x = v.split(" ")
+        a = x[-3]
+        if len(a) == 0:
+            print(x[-2])
+        else:
+            #print(x[-3:])
+            print((x[-3])[11:])
+            self.l.setText(x[-3][11:]+" kg")
+
+app = QtWidgets.QApplication(sys.argv)
+a_window = Window()
+sys.exit(app.exec_())
