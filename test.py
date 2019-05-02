44bb8946bf0a7e12aa08c96c604a175b49893142
kalaistark <kalaiselvant0@gmail.com>
2019-05-02 11:02:10 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,46 @@
 import sys, subprocess, serial, csv
-import os
+import os,glob
 import datetime as time
 from PyQt5 import QtWidgets
 
-port = "/dev/ttyUSB0"
 
+result = []
 # subprocess.run("chmod 666 /dev/ttyUSB0")
-returned = os.popen("sudo chmod 666 /dev/ttyUSB0").readline()
-print(returned)
+def serial_ports():
+   if sys.platform.startswith('win'):
+       ports = ['COM%s' % (i + 1) for i in range(256)]
+   elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+       # this excludes your current terminal "/dev/tty"
+       ports = glob.glob('/dev/tty[A-Za-z]*')
+   elif sys.platform.startswith('darwin'):
+       ports = glob.glob('/dev/tty.*')
+   else:
+       raise EnvironmentError('Unsupported platform')
 
-ser = serial.Serial("/dev/ttyUSB0", 2400, timeout=2)
+   for port in ports:
+       try:
+           s = serial.Serial(port)
+           s.close()
+           result.append(port)
+       except (OSError, serial.SerialException):
+           pass
+   return result
+
+
+
+def admin():
+    serial_ports()
+    command = "sudo chmod 666 "+result[0]
+    password = "romeo123"
+    p = os.system('echo %s|sudo -S %s' % (password, command))
+
+admin()
+ser = serial.Serial(result[0], 2400, timeout=2)
 
 
 #dmesg | grep tty -- to list serial ports in linux
 # sudo chmod 666 /dev/ttyUSB0)
+
 
 class Window(QtWidgets.QWidget):
 
@@ -43,6 +70,7 @@ class Window(QtWidgets.QWidget):
         self.show()
 
     def get_weight(self):
+        csv = open("weightdata.csv","a")
         ser.flushOutput()
         bytesToRead = ser.inWaiting()
         v= str(ser.read(bytesToRead))
@@ -50,9 +78,14 @@ class Window(QtWidgets.QWidget):
         a = x[-3]
         if len(a) == 0:
             print(x[-2])
+            x = (x[-2])[11:]
+            csv.write(x + "kg" + " time:" + str(time.datetime.now())+ "\n")
+            self.l.setText(x)
         else:
             #print(x[-3:])
             print((x[-3])[11:])
+            y = (x[-3])[11:]
+            csv.write(y +" kg" + " time:" + str(time.datetime.now())+ "\n")
             self.l.setText(x[-3][11:]+" kg")
 
 app = QtWidgets.QApplication(sys.argv)
